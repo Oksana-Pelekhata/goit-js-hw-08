dbaf09ced8e9f3e7aae4ea319a51f11d58c62494
Oksana-Pelekhata <ok.soloviova@gmail.com>
2023-03-13 00:29:48 +0200

    task-1-updated

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,52 +8,20 @@ const galleryContainer = document.querySelector(".gallery")
 
 function createGalleryItemsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
     }).join('')
 }
-
 const galleryMarkup = createGalleryItemsMarkup(galleryItems)
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
 
-galleryContainer.addEventListener('click', handleImgClick)
-
-function handleImgClick(event) {
-    event.preventDefault();
-
-
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    
-    const largeImageUrl = event.target.dataset.source
-    const instance = basicLightbox.create(
-        `<img src="${largeImageUrl}" width="800" height="600">`
-    )
-    instance.show(() => {
-    window.addEventListener('keydown', onEscKeyPress)
-    })
-
-    function onEscKeyPress(event) {
-        const ESC_KEY_CODE = 'Escape';
-        const isEscKey = event.code === ESC_KEY_CODE;
-
-        if (isEscKey) {
-            instance.close(() => {
-            window.removeEventListener('keydown', onEscKeyPress)
-            })
-        }
-    }
-
-}
+    let gallery = new SimpleLightbox(".gallery a", {
+        captionSelector: 'img',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        scrollZoom: false,
+    });
 
 console.log(galleryItems);
-
