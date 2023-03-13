3a79f9f27bb52d30cc642c76444b9c5987879363
Oksana-Pelekhata <ok.soloviova@gmail.com>
2023-03-13 02:54:04 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const currentTimeCatchUp = player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+}, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSON.parse(savedTime);
+
+player.setCurrentTime(parsedTime)
+
+
 
