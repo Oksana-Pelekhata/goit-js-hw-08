d2e75ad6283fedca59cfa0838c48b3e1e711df99
Oksana-Pelekhata <ok.soloviova@gmail.com>
2023-03-14 04:45:29 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form')
+const inputEl = document.querySelector('.feedback-form  input')
+const textareaEl = document.querySelector('.feedback-form  textarea')
+
+const formData = {
+    email: '',
+    message: '',
+};
+
+
+formEl.addEventListener('input', throttle(handleFormElMessage, 500))
+formEl.addEventListener('submit', handleFormSubmit)
+
+populateTextarea();
+
+function handleFormElMessage(e) {
+    formData[e.target.name] = e.target.value;
+    console.log(formData)
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem('feedback-form-state');
+    const parsedMessage = JSON.parse(savedMessage);
+
+    if (parsedMessage) {
+            inputEl.value = parsedMessage.email;
+            textareaEl.value = parsedMessage.message;
+    }
+}
+
+function handleFormSubmit(e) {
+    e.preventDefault();
+    
+    console.log(formData)
+    e.target.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
 
